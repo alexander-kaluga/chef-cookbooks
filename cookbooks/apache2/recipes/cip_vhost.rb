078cc31f8a569bcbb05aad20bd3a218a000524b4
Alexander Kaluga <alexkaluga@gmail.com>
2013-04-29 11:27:43 -0700

    updated doc root path

--- cookbooks/apache2/recipes/cip_vhost.rb
+++ cookbooks/apache2/recipes/cip_vhost.rb
@@ -2,5 +2,5 @@
 web_app_cip "cip-blowfish" do
   server_name "blowfish.local"
   server_aliases ["*.blowfish.local"]
-  docroot "/home/vagrant/www/blowfish/symfony/web"
+  docroot "/home/vagrant/www/blowfish/web"
 end
